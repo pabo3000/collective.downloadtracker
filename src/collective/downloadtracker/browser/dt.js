88f7b0e0a5244c6ea3d9dc20c04b982daf7aec10
Pavel Bogdanovic <pb@riboli.eu>
2016-02-11 13:24:05 +0100

    replace jq by $. Ajaxify downloadtracker view.

--- src/collective/downloadtracker/browser/dt.js
+++ src/collective/downloadtracker/browser/dt.js
@@ -1,24 +1,24 @@
-jq(document).ready(function(){
-	jq('.documentByLine td a').click(function() {
-		var td = jq(this).parent()
-		var name = td.prev().prev().text();
-		var date = td.prev().text();
-		var url = window.location.href.replace(/view/, "delete");
-		// alert('Handler for .click() called.');
-		jq.getJSON(
-			url,
-			{
-				name: name,
-				date: date
-			},
-			function(data) {
-				// im Erfolgsfall Seite neuladen
-				if (data==true) {
-					location.reload();
-				}
-			}
-		);
-		return false;
-	});
- });
-
+$(document).ready(function() {
+  $('.documentByLine td a').click(function() {
+    var td = $(this).parent();
+    var tr = td.parent();
+    var name = td.prev().prev().text();
+    var date = td.prev().text();
+    var url = window.location.href.replace(/view/, "delete");
+    // alert('Handler for .click() called.');
+    $.getJSON(
+      url, {
+        name: name,
+        date: date
+      },
+      function(data) {
+        // im Erfolgsfall Seite neuladen
+        if (data === true) {
+          tr.remove();
+          //location.reload();
+        }
+      }
+    );
+    return false;
+  });
+});
